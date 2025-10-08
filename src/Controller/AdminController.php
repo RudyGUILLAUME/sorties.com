8f55f15b788505a31e7f17939f2fedd48d84c836
Lucas-eni <lucas.robichon2025@campus-eni.fr>
2025-10-08 16:42:19 +0200

    Ajout participants par fichier csv
    Ajout de plusieurs participants par fichier csv pour les admins

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,8 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
+use App\Form\ImportCsvType;
+use App\Repository\SiteRepository;
+use App\Service\FichierCSVService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class AdminController extends AbstractController
@@ -16,5 +23,31 @@ final class AdminController extends AbstractController
         }
 
         return $this->render('admin/index.html.twig');
+    }
+
+    #[Route('/admin/import', name: 'admin_import_csv')]
+    public function importCsv(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $passwordHasher,FichierCSVService $csv): Response
+    {
+        if (!$this->isGranted('ROLE_ADMIN')) {
+            return $this->redirectToRoute('app_home');
+        }
+
+        $form = $this->createForm(ImportCsvType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $file = $form->get('csv_file')->getData();
+
+            if ($file) {
+                $count=$csv->createUsers($file->getPathname(),$em,$passwordHasher);
+
+                $this->addFlash('success', "$count utilisateurs importés avec succès !");
+                return $this->redirectToRoute('admin_import_csv');
+            }
+        }
+
+        return $this->render('admin/import.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
