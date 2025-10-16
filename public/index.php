997161e33634e13c3ac8787f15849e3f1bddab34
RudyGUILLAUME <123960248+RudyGUILLAUME@users.noreply.github.com>
2025-10-16 14:44:32 +0200

    deploy

--- public/index.php
+++ public/index.php
@@ -1,8 +1,13 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\HttpFoundation\Request;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+if (PHP_SAPI === 'cli-server' || PHP_SAPI === 'frankenphp') {
+    $_SERVER['SCRIPT_FILENAME'] = __FILE__;
+}
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
